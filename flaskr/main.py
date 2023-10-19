e2fbde8761b9b10b8ad021d60e40ec8e181388a1
mb <nikotamajapan@gmail.com>
2023-10-19 18:28:39 +0900

    spell miss on 17

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,7 +4,23 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    books = [
+        {
+        'title':'harapeko',
+        'price':1000,
+        'arrival_day':'2020/7/12'
+    },
+    {
+        'title':'harapeko',
+        'price':1000,
+        'arrival_day':'2020/7/12'
+    }
+    ]
+
+
+
     return render_template(
         'index.html',
+        books=books
     )
 
